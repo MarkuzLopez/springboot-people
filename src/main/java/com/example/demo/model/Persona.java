73f222d45a79e6b484fd887323a143c17d740338
Marco Lopez <marcolopez@MacBook-Pro-de-Marco.local>
2025-09-19 13:48:27 -0600

    documentation of JPA and hibernate with springboot

--- src/main/java/com/example/demo/model/Persona.java
+++ src/main/java/com/example/demo/model/Persona.java
@@ -5,6 +5,8 @@ import lombok.*;
 
 /** 
  * PASO 3 CREAR ENTIDAD PERSONA USANDO LOOMBOK EVITANDO boilerplate
+ * @Entity: clase Java que representa una tabla en la base de datos.
+ * Se marca con la anotación @Entity para que JPA/Hibernate la reconozca.
  * **/
 
 @Entity
@@ -13,11 +15,20 @@ import lombok.*;
 @AllArgsConstructor 	// Constructor con tofoso los campos
 
 public class Persona {
+	/**
+	 * @Id: Este campo será el identificador único de cada registro en la tabla.
+	 * @@GeneratedValue: para indicar cómo se generan los valores del @Id.
+	 * Comúnmente se usa para que la base de datos asigne un valor auto incremental.
+	 * @GenerationType.IDENTITY: -> Delega la generación al motor de BD (ejemplo: MySQL autoincrement).
+	 * @GenerationType.SEQUENCE → Usa una secuencia (más común en Oracle/PostgreSQL).
+	 * @GenerationType.AUTO → Hibernate elige automáticamente la estrategia según la BD.
+	 */
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id // clave primaria
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //  auto incremental
 	private Long id;
 	
+	@Column(nullable = false, length = 100) // Opcional: para personalizar columna
 	private String nombre;
 	private int edad;	
 }
--- src/main/java/com/example/demo/repository/PersonaRepository.java
+++ src/main/java/com/example/demo/repository/PersonaRepository.java
@@ -7,5 +7,26 @@ import com.example.demo.model.Persona;
  * PASO 4 Crear repository, implementando interface usando JPA
  */
 
+/**
+ * En Spring Data JPA, un repositorio es una interfaz que permite acceder 
+ * y manipular datos en la base de datos.
+ * 
+ * CrudRepository: Proporciona operaciones CRUD (Create, Read, Update, Delete).
+ * metodos principales: save(S entity) → guarda o actualiza una entidad.	
+	- findById(ID id) → busca un registro por ID.	
+	- findAll() → devuelve todos los registros.	
+	- deleteById(ID id) → elimina un registro por ID.	
+	- count() → devuelve el número de registros.
+	- existsById(ID id) → comprueba si existe un registro con un ID específico.
+ * 	
+ * JpaRepository: Extiende de CrudRepository y PagingAndSortingRepository.
+ * Agrega más funcionalidades, como paginación, ordenación y consultas personalizadas.
+  	- findAll(Sort sort) → devuelve todos los registros con ordenamiento.
+  	- findAll(Pageable pageable) → devuelve resultados paginados.
+	- flush() → sincroniza con la base de datos inmediatamente.
+	- saveAndFlush(entity) → guarda y hace flush.
+	- deleteAllInBatch() → elimina en lote.
+ */
+
 public interface PersonaRepository extends JpaRepository<Persona, Long> {
 }
